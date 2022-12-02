6bb03055680ad3506302339332e543bb0d3edfed
Lydia Eluro <elurolydia@gmail.com>
2022-12-02 17:57:55 +0100

    added holdDice function

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,57 @@
 import React from "react";
 import Die from "./components/Die";
+import {nanoid} from "nanoid" 
 
 
 
 export default function App () {
 
-  const allNewDice = () =>{
+
+  const [num, setNum] = React.useState(allNewDice());
+
+  function allNewDice(){
     let arr = []
-    while (arr.length < 10) {
-        let y = Math.floor((Math.random() * 6) + 1)
-        arr.push(y)
+
+    for (let i = 0; i < 10; i++){
+      let y = {
+        value: Math.floor((Math.random() * 6) + 1), 
+        isHeld: false,
+        id: nanoid()
+      }
+      
+      arr.push(y)
     }
-    
     return arr
   }
 
 
-  const [num, setNum] = React.useState(allNewDice());
-   
-  const value = num.map(val => {
-    return <Die value = {val}/>
-  })
+  function btnCall () { 
+    setNum(allNewDice())
+  }
+  
+  function holdDice (id)  {
+
+    setNum(prevNum => prevNum.map(die => {
+        return (die.id === id ? 
+          {...die, isHeld: !die.isHeld} :
+          die)
+      })
+    )
+
+  };
 
   
+   
+  const value = num.map(val => (
+    <Die 
+        key={val.id} 
+        value = {val.value} 
+        isHeld={val.isHeld} 
+        id = {val.id} 
+        holdDice={holdDice}
+    />
+  ))
+
   return (
     <div id="app"> 
       
@@ -32,8 +61,12 @@ export default function App () {
                 <p>Roll until all dice are the same. 
                     Click each die to freeze it at its current value between rolls.
                 </p>
-                <div id="dice">{value}</div>
-                <button id="rollbtn">Roll</button>
+
+                <div id="dice"> 
+                  {value} 
+                </div>
+
+                <button id="rollbtn" onClick={btnCall}>Roll</button>
             </div>
         </main>
 
--- src/components/Die.jsx
+++ src/components/Die.jsx
@@ -3,7 +3,15 @@ import React from 'react'
 const Die = (props) => {
   return (
     <div>
-        <div>{props.value}</div>
+        <div 
+          id="die" 
+          style={{backgroundColor: props.isHeld? '#59E391': '#FFFFFF'}} 
+          onClick = {()=> {
+            props.holdDice(props.id)
+          }}
+        >
+            {props.value}
+          </div>
     </div>
   )
 }
